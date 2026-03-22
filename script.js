432d764c24ec494d38e2722f043515f29ff72365
OSIRI MANG OSIRI <osiri.mang.211278@fuoye.edu.ng>
2026-03-22 22:15:31 +0100

    Perpetual Logistics
    This is a Delivery Package Website, created by Ositex Creative Hub

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("startBtn").addEventListener("click", function () {
+    document.querySelector(".landing").style.display = "none";
+    document.querySelector(".form-section").style.display = "block";
+});
