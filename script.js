12f37817a2eb19a78db686db1a6d7c4417f41d86
OSIRI MANG OSIRI <osiri.mang.211278@fuoye.edu.ng>
2026-03-24 09:35:12 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("startBtn").addEventListener("click", function () {
-    document.querySelector(".landing").style.display = "none";
-    document.querySelector(".form-section").style.display = "block";
-});
